fe6fd06fbbc89c59af370d0a2feb18dc99b61a81
tuwanli <909111806@qq.com>
2016-12-15 16:20:45 +0800

    Initial Commit

---
+++ VideoDemoToSun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoDemoToSun
+//
+//  Created by 涂婉丽 on 16/12/15.
+//  Copyright © 2016年 涂婉丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoDemoToSun/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoDemoToSun
+//
+//  Created by 涂婉丽 on 16/12/15.
+//  Copyright © 2016年 涂婉丽. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
